8c99a8419e7737af325133727c5b4c339f9e44d6
Ilmars Viksne <ilmars.viksne@rtu.lv>
2023-11-16 23:42:35 +0200

    Stereo data processing.

--- useLib/useLib.cpp
+++ useLib/useLib.cpp
@@ -16,17 +16,18 @@ int main() {
        audio.readAudioData();
 
        // Wait for a while to let the child thread finish reading audio data
-       std::this_thread::sleep_for(std::chrono::seconds(3));
+       std::this_thread::sleep_for(std::chrono::seconds(1));
 
        // Get the read audio data
        const std::vector<float>& audioData = audio.getAudioData();
 
-       // Print the first 10 data points
-       for (size_t i = 0; i < 10 && i < audioData.size(); i++) {
-           std::cout << "Data point " << i << ": " << audioData[i] << std::endl;
+       // Print data points
+       for (size_t i = 0; i < audioData.size(); i += 2 ) {
+           std::cout << "[" << i/2 << "] " << audioData[i] << "   " << audioData[i+1] << std::endl;
        }
 
     }
+
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << '\n';
     }
